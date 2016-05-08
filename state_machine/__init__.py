bd49d67cff6f2812be4c6c4517311f93153f2710
jonathan.hon <jonathan.hon@hotmail.com>
2016-05-08 18:26:17 +0100

    StateMachine-6 Refactor data into events

--- state_machine/__init__.py
+++ state_machine/__init__.py
@@ -1,5 +1,7 @@
 
 from state_machine.state_machine import StateMachine
 from state_machine.state import State, FinalState
-from state_machine.condition import Condition, AlwaysTrue, AlwaysFalse, CompositeOrCondition, CompositeAndCondition
+from state_machine.transition import Transition, DefaultTransition
+from state_machine.condition import Condition,AlwaysTrue, AlwaysFalse, CompositeOrCondition, CompositeAndCondition
 from state_machine.model import Model
+from state_machine.event import Event
