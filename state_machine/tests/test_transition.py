f1ad6ec2026f160beabd588978aabbe911e37d1f
jonathan.hon <jonathan.hon@hotmail.com>
2016-05-08 18:13:02 +0100

    StateMachine-6 Refactor data into events

--- state_machine/tests/test_transition.py
+++ state_machine/tests/test_transition.py
@@ -1,25 +1,21 @@
 import unittest
-from state_machine.transition import Transition
-from state_machine.constants import CURRENT_STATE_NAME, CURRENT_STATE_COMMENT, CURRENT_STATE_INPUT, CURRENT_STATE_START_RESULT
+from state_machine import Transition, Event
 from mock import MagicMock
 from state_machine.condition import ALWAYS_TRUE, ALWAYS_FALSE
 
 class TransitionTests(unittest.TestCase):
 
     def test__init__(self):
-        source = MagicMock()
         target = MagicMock()
         conditions = []
-        t = Transition( source, target, conditions )
+        t = Transition(target, conditions)
 
     def test_is_triggered_true(self):
-        source = MagicMock()
         target = MagicMock()
-        t = Transition( source, target, ALWAYS_TRUE )
-        self.assertTrue( t.is_triggered( None, None ) )
+        t = Transition(target, ALWAYS_TRUE)
+        self.assertTrue( t.is_triggered(Event()) )
 
     def test_is_triggered_false(self):
-        source = MagicMock()
         target = MagicMock()
-        t = Transition( source, target, ALWAYS_FALSE )
-        self.assertFalse( t.is_triggered( None, None ) )
+        t = Transition(target, ALWAYS_FALSE)
+        self.assertFalse( t.is_triggered(Event()) )
--- state_machine/transition.py
+++ state_machine/transition.py
@@ -1,17 +1,16 @@
 
 class Transition( object ):
 
-    def __init__( self, source_state, target_state, condition ):
-        self.source = source_state
+    def __init__( self, target_state, condition ):
         self.target = target_state
         self.condition = condition
 
     def __repr__(self):
         condition_description = self.condition_description()
-        return 'Transition from %s to %s, %s'%(self.source,self.target,condition_description)
+        return 'Transition from to %s, %s'%(self.target,condition_description)
 
     def condition_description(self):
         return 'Condition:(%s)'%self.condition.name
 
-    def is_triggered(self, data, model, start_result=None ):
-        return self.condition.is_triggered(data, model)
+    def is_triggered(self, event):
+        return self.condition.is_triggered(event)
