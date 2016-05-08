7ac70d45abf395e897136e35cecbdcc9fe83efab
jonathan.hon <jonathan.hon@hotmail.com>
2016-05-08 22:07:40 +0100

    StateMachine-6 Refactor data into events

--- state_machine/tests/test_state_machine.py
+++ state_machine/tests/test_state_machine.py
@@ -6,6 +6,7 @@ from state_machine.exception import StateMachineException
 from state_machine.state import State
 from state_machine.condition import ALWAYS_TRUE, ALWAYS_FALSE, AlwaysTrue
 from state_machine.model import Model
+from state_machine import Event
 from mock import MagicMock
 
 
@@ -27,35 +28,13 @@ class StateMachineTests(unittest.TestCase):
         with self.assertRaises( StateMachineException ):
             sm.add_state( state )
 
-    def test_run(self):
-        data  = None
-        model = MagicMock()
+    def test_handle_event(self):
         sm = StateMachine( 'test machine' )
-        sm._run = MagicMock()
-        sm.run( data, model )
+        sm.handle_event( Event() )
 
-    def test_run_logs_exception(self):
-        data  = None
-        model = MagicMock()
-        sm = StateMachine( 'test machine' )
-        sm._run = MagicMock( side_effect=StateMachineException( 'intentional' ) )
-
-        sm.run( data, model )
-
-        self.assertEqual( 1, model.logger.error.call_count )
-
-    def test_run_without_current_state(self):
-        sm = StateMachine( 'state machine 1' )
-        model = MagicMock()
-        model.current_state = None
-
-        with self.assertRaises( StateMachineException ) as e:
-            sm._run(None, model)
-        self.assertEqual( 'Model has no current state', e.exception.msg )
-
-    def test_run_simple(self):
+    def test_handle_event_simple(self):
         '''
-        A simple test from state1 to state2
+        A simple example of using a state machine
         '''
 
         sm = StateMachine( 'state machine 1' )
@@ -65,54 +44,49 @@ class StateMachineTests(unittest.TestCase):
 
         src.add_transition_to( tgt, ALWAYS_TRUE )
 
-        model = Model( 'test_model' )
-        model.set_state(src)
-
-        data = {ALWAYS_TRUE.listens_for:1}
+        sm.set_state(src)
+        evt  = Event(ALWAYS_TRUE.listens_for)
 
         # TEST
-        sm.run(data, model)
+        sm.handle_event(evt)
 
         # VERIFY
-        self.assertEqual(tgt, model.current_state)
+        self.assertEqual(tgt, sm.current_state)
 
-    def test_run_passes_start_result(self):
-        # # Need to be able to send start_result to the next state
+
+    def test_handle_event_handles_start_result(self):
+        # # Need to be able to check that if on_start returns an event, then we handle it as well
         # e.g.
-        #     state place book order will check inventory straight away
-        #         -   no inventory
-        #         -   has inventory
-
-        sm = StateMachine( 'state machine 1' )
+        #     order a pizza
+        #     get back a pizza order event
+        #     handle the pizza order event
 
         state_wait = State( 'Wait State' )
 
         state_action = State('Action State')
-        state_action.on_start = lambda *arg, **kwargs: 'action result'
+        state_action.on_start = lambda event: Event(name='action',payload='hello')
 
         state_final = State('Final State')
-        state_final.on_start = lambda data, model, state_input:{'final state input':state_input}
+        state_final.on_start = MagicMock()
 
         state_wait.add_default_transition_to(state_action)
         state_action.add_default_transition_to(state_final)
 
-        model = Model( 'test_model' )
-        model.set_state(state_wait)
+        sm = StateMachine( 'state machine 1' )
+        sm.set_state(state_wait)
 
-        data = {ALWAYS_TRUE.listens_for:1}
-        sm.run(data, model)
+        evt = Event()
+        sm.handle_event(evt)
 
-        model.logger.info( 'We are in state %s',model.current_state)
-        model.logger.info( 'State input ''%s''',model.current_state_input)
-        self.assertEqual( 'action result', model.current_state_input)
-        self.assertEqual( {'final state input':'action result'}, model.current_state_start_result)
+        sm.logger.info( 'We are in state %s',sm.current_state)
+        self.assertEqual( state_final, sm.current_state)
+        self.assertEqual('action', state_final.on_start.call_args[0][0].name)
+        self.assertEqual('hello', state_final.on_start.call_args[0][0].payload)
 
     def test_run_splits_streams_correctly(self):
 
-        sm = StateMachine('test machine')
-
-        src = State( 'src' )
-        tgt1 = State( 'tgt1' )
+        src  = State('src')
+        tgt1 = State('tgt1')
         tgt2 = State('tgt2')
 
         c1  = AlwaysTrue()
@@ -124,23 +98,22 @@ class StateMachineTests(unittest.TestCase):
         src.add_transition_to( tgt1, c1 )
         src.add_transition_to( tgt2, c2 )
 
-        model = Model( 'test' )
+        sm = StateMachine('test machine')
 
         # Test1
-        model.set_state(src)
-        sm.run( {'stream1':1}, model )
-        self.assertEqual(tgt1, model.current_state)
+        sm.set_state(src)
+        sm.handle_event(Event('stream1'))
+        self.assertEqual(tgt1, sm.current_state)
 
         # Test2
-        model.set_state(src)
-        sm.run( {'stream2':1}, model )
-        self.assertEqual(tgt2, model.current_state)
+        sm.set_state(src)
+        sm.handle_event(Event('stream2'))
+        self.assertEqual(tgt2, sm.current_state)
 
         # Test3
-        model.set_state(src)
-        sm.run( {'none':1}, model )
-        self.assertEqual(src, model.current_state)
-
+        sm.set_state(src)
+        sm.handle_event(Event())
+        self.assertEqual(src, sm.current_state)
 
 
 if __name__ == "__main__":
