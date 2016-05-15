8c97b6a676e41918db3282ec99017f909e12a8c0
jonathan.hon <jonathan.hon@hotmail.com>
2016-05-15 21:13:11 +0100

    Delayed observable

--- state_machine/tests/events/test_observer.py
+++ state_machine/tests/events/test_observer.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from state_machine.events import Event, Observer, Observable
+from state_machine.events import Event, Observer, Observable, DelayedObservable
 from mock import MagicMock
 
 class ObserverableTests(TestCase):
@@ -32,4 +32,25 @@ class ObserverableTests(TestCase):
 
         e = Event('baked')
         observable.notify_observers(e)
+        self.assertEqual(1, observer.notify.call_count)
+
+
+class DelayedObservableTests(TestCase):
+
+    def test_notify_observers(self):
+        observer = Observer()
+        observer.notify = MagicMock()
+
+        observable = DelayedObservable()
+        observable.register_observer('baked',observer)
+
+        e = Event('cooked')
+        observable.notify_observers(e)
+        self.assertEqual(0, observer.notify.call_count)
+
+        e = Event('baked')
+        observable.notify_observers(e)
+        self.assertEqual(0, observer.notify.call_count)
+
+        observable.flush()
         self.assertEqual(1, observer.notify.call_count)
