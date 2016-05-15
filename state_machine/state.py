5c706ee0735e27cc78e7b5d0d233a894eb510525
jonathan.hon <jonathan.hon@hotmail.com>
2016-05-15 17:47:12 +0100

    moved class attributes into constructors to make them no static,

--- state_machine/state.py
+++ state_machine/state.py
@@ -4,19 +4,23 @@ from collections import defaultdict
 
 class BaseState( object ):
 
-    start_event = None
-
-    on_start = None
-    on_end = None
-
     def __init__(self, name, machine):
         self.name=name
         self.machine=machine
         self.default_transition=None
-        self.vars = {}
+
+        self.start_event = None
+
+        self.on_start = None
+        self.on_end = None
+        self._vars = {}
 
     def __repr__(self):
         return 'State:%s'%self.name
+
+    @property
+    def vars(self):
+        return self._vars
 
     @property
     def logger(self):
@@ -26,23 +30,19 @@ class BaseState( object ):
         self.machine.notify_observers(event)
 
     def add_default_transition_to(self, target):
-        """
-        Add a default transition to target
-        :param target:  the target state
-        :return:
-        """
         self.default_transition = DefaultTransition(target)
 
     def add_transition_to(self, target, trigger=None, guard=None, action=None):
         pass
 
     def start(self, event):
+        self._vars.clear()
         self.start_event=event
         if self.on_start:
             self.on_start(event,self)
 
     def end(self, event):
-        self.vars = {}
+        self._vars.clear()
         if self.on_end:
             self.on_end(event, self)
 
@@ -58,13 +58,9 @@ class BaseState( object ):
 
 class State(BaseState):
 
-    start_event = None
-
     def __init__(self, name, machine):
         super(State, self).__init__(name, machine)
         self.listeners = defaultdict(list)
-        self.default_transition=None
-        self.vars = {}
 
     def __repr__(self):
         return 'State:%s'%self.name
@@ -82,11 +78,8 @@ class State(BaseState):
 
         transitions = self.listeners[event.name]
         for t in transitions:
-            #model.logger.info('Testing transition %s',t)
             if t.is_triggered(event):
-                #model.logger.info( 'Returning %s',t)
                 return t
-
         return self.default_transition
 
 
