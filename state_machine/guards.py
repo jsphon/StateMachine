1f0e3b4e464f79f64a39586ae04e4fd3b55a8de1
jonathan.hon <jonathan.hon@hotmail.com>
2016-05-30 11:47:23 +0100

    Added is_child_final

--- state_machine/guards.py
+++ state_machine/guards.py
@@ -2,3 +2,13 @@ from state_machine import FinalState
 
 def is_final_state(event, state):
     return isinstance(state, FinalState)
+
+
+def is_child_final(event, state):
+    """
+
+    :param event:
+    :param state: Composite State
+    :return: True if state's child is Final
+    """
+    return isinstance(state.current_state, FinalState)
--- state_machine/tests/test_guards.py
+++ state_machine/tests/test_guards.py
@@ -1,6 +1,6 @@
 import unittest
 import state_machine.guards as guards
-from state_machine import FinalState, StateMachine, Event
+from state_machine import FinalState, StateMachine, Event, CompositeState
 
 class GuardTests(unittest.TestCase):
 
@@ -19,4 +19,20 @@ class GuardTests(unittest.TestCase):
         s = m.create_state('final', AnotherFinalState)
         e = Event()
         self.assertTrue( guards.is_final_state(e, s) )
+
+    def test_is_child_final_true(self):
+        m=StateMachine()
+        s=m.create_state('final', CompositeState)
+        f=s.create_state('sfinal', FinalState)
+        s.set_state(f)
+        e=Event()
+        self.assertTrue( guards.is_child_final(e, s))
+
+    def test_is_child_final_false(self):
+        m=StateMachine()
+        s=m.create_state('final', CompositeState)
+        c=s.create_state('s')
+        s.set_state(c)
+        e=Event()
+        self.assertFalse( guards.is_child_final(e, s))
 
