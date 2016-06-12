2d700bb1946c2d1f93b11db078cdf99e0a79dc4a
jonathan.hon <jonathan.hon@hotmail.com>
2016-06-12 22:04:55 +0100

    Created the fixed duration machine.

--- state_machine/guards.py
+++ state_machine/guards.py
@@ -12,3 +12,15 @@ def is_child_final(event, state):
     :return: True if state's child is Final
     """
     return isinstance(state.current_state, FinalState)
+
+
+def tick_before(dt):
+    def inner_func(event, state):
+        return event.payload<dt
+    return inner_func
+
+
+def tick_after(dt):
+    def inner_func(event, state):
+        return event.payload>dt
+    return inner_func
