26d6db442f29d32591a185cd084138c61dd2e2e0
Jonathan Hon <jonathanhon@hotmail.com>
2016-12-01 22:57:49 +0000

    added timeout

--- state_machine/tests/test_unique_process_instance.py
+++ state_machine/tests/test_unique_process_instance.py
@@ -1,13 +1,17 @@
 from unittest import TestCase
 from state_machine.stoppable_thread import UniqueStoppableThread
-from state_machine.unique_process_instance import UniqueProcessInstance, ProcessExistsException
+from state_machine.unique_process_instance import UniqueProcessInstance, ProcessExistsException, DateTime, TIMEOUT_SECONDS
+from state_machine import unique_process_instance
+from unittest.mock import patch
 import uuid
 import os
 import time
+from datetime import datetime, timedelta
 
 import logging
 logger = logging.getLogger('default')
 logger.addHandler(logging.StreamHandler())
+
 
 class MockThread(UniqueStoppableThread):
 
@@ -68,3 +72,27 @@ class TestUniqueStoppableThread(TestCase):
             self.assertTrue(os.path.exists(self.instance.pid_folder))
 
         self.assertFalse(os.path.exists(self.instance.pid_folder))
+
+    @patch('state_machine.unique_process_instance.DateTime')
+    def test_create_timeout(self, mock_date_time):
+
+        #SETUP
+        os.makedirs(self.instance.pid_folder)
+        utcnow = datetime.utcnow()
+        mock_date_time.utcnow.return_value = utcnow
+        expected_timeout = utcnow+timedelta(seconds=TIMEOUT_SECONDS)
+
+        #TEST
+        self.instance.create_timeout()
+
+        # VERIFY
+        self.assertPathExists(self.instance.timeout_file)
+        with open(self.instance.timeout_file, 'r') as f:
+            timeout_str = f.read()
+
+        timeout = datetime.strptime(timeout_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+        self.assertEqual(expected_timeout, timeout)
+
+
+    def assertPathExists(self, path):
+        self.assertTrue(os.path.exists(path))
--- state_machine/unique_process_instance.py
+++ state_machine/unique_process_instance.py
@@ -3,12 +3,13 @@ Unique instance of a process, defined by id
 
 """
 
+from datetime import datetime, timedelta
 import errno
 import getpass
 import os
 
 FOLDER = '/home/%s/state_machine/'%getpass.getuser()
-
+TIMEOUT_SECONDS = 300
 
 class ProcessExistsException(Exception):
     pass
@@ -19,6 +20,7 @@ class UniqueProcessInstance(object):
     def __init__(self, id):
         self.id = id
         self.pid_folder = os.path.join(FOLDER, id)
+        self.timeout_file = os.path.join(self.pid_folder, 'timeout')
 
     def __enter__(self):
         self.start()
@@ -39,3 +41,11 @@ class UniqueProcessInstance(object):
 
     def _remove_pid_folder(self):
         os.rmdir(self.pid_folder)
+
+    def create_timeout(self, timeout_seconds=TIMEOUT_SECONDS):
+        timeout = DateTime.utcnow() + timedelta(seconds=timeout_seconds)
+        with open(self.timeout_file, 'w') as f:
+            f.write(timeout.strftime('%Y-%m-%dT%H:%M:%S.%fZ'))
+
+class DateTime(datetime):
+    pass
