3a33e36c0b2a77375eca04a1610728a1335a0693
Jonathan Hon <jonathanhon@hotmail.com>
2017-01-01 21:39:41 +0000

    commented out dependency on pygraphviz

--- state_machine/__init__.py
+++ state_machine/__init__.py
@@ -1,8 +1,9 @@
+from state_machine.composite_state import CompositeState
 from state_machine.condition import Condition,AlwaysTrue, AlwaysFalse, CompositeOrCondition, CompositeAndCondition
+from state_machine.events.event import Event
+from state_machine.exception import StateMachineException
 from state_machine.state import State, FinalState, PseudoState
 from state_machine.state_machine import StateMachine
 from state_machine.transition import Transition, DefaultTransition
-from state_machine.composite_state import CompositeState
-from state_machine.exception import StateMachineException
-from state_machine.events.event import Event
-from state_machine.diagrams import display_machine
+
+# from state_machine.diagrams import display_machine
